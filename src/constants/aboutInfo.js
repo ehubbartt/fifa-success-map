198a9c93ac902c06fda88a6ef8fa580278eedc65
Chalsea Chen <chal2000@uw.edu>
2022-06-07 15:07:47 -0700

    add choro

--- src/constants/aboutInfo.js
+++ src/constants/aboutInfo.js
@@ -1,6 +1,6 @@
 export const aboutInfo = {
   goal: "The project was designed to observe the success of the FIFA World Cup in terms of country size and player size. Visualize matches over time with maps that focus on players, teams, and matches. The project hopes to provide some insights into the success or failure of the World Cup in different countries. It also provides a way to look at the success of the players and the history of the game.",
   methodology: "",
-  dataSources: <a href="https://www.kaggle.com/datasets/abecklas/fifa-world-cup?resource=download&select=WorldCups.csv">Team/Match Data</a>,
+  dataSources: "",
   acknowledgements: "",
 };
