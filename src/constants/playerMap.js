e838633d01700cd144f326ad78f4231be6384ba8
Chalsea Chen <chal2000@uw.edu>
2022-06-07 14:58:56 -0700

    added choro

--- src/constants/playerMap.js
+++ src/constants/playerMap.js
@@ -1,62 +1,53 @@
-// import { players_data } from '../assets/data/players_data.js';
+import { totalplayers_data } from '../assets/data/totalplayers_data.js';
+const layers = [
+'0-49',
+'50-99',
+'100-149',
+'150-199',
+'200-249',
+'250+'
+];
+const colors = [
+'#c6dbef',
+'#9ecae1',
+'#6baed6',
+'#4292c6',
+'#2171b5',
+'#08306b'
+];
+export const playerMap = {
+  name: "playerMap",
+  title: "player",
+  style: "mapbox://styles/mapbox/dark-v10",
+  data: totalplayers_data,
+  center: [0, 16],
+  zoom: 1,
+  projection: "mercator",
+  id:"do_not_change_this_value",
 
-// const grades = [1000, 10000, 50000],
-//   colors = ["rgb(208,209,230)", "rgb(103,169,207)", "rgb(1,108,89)"],
-//   radii = [7, 7, 7];
-
-// export const playerMap = {
-//   name: "playerMap",
-//   title: "player",
-//   style: "mapbox://styles/mapbox/dark-v10",
-//   data: players_data,
-//   center: [0, 16],
-//   zoom: 1,
-//   projection: "mercator",
-//   id:"do_not_change_this_value",
-
-//   layerOptions: {
-//     id: "do_not_change_this_valu",
-//     type: "circle",
-//     source: "do_not_change_this_value",
-//     paint: {
-//       // increase the radii of the circle as the zoom level and dbh value increases
-//       "circle-radius": {
-//         property: "Attendance",
-//         stops: [
-//           [
-//             {
-//               zoom: 5,
-//               value: grades[0],
-//             },
-//             radii[0],
-//           ],
-//           [
-//             {
-//               zoom: 5,
-//               value: grades[1],
-//             },
-//             radii[1],
-//           ],
-//           [
-//             {
-//               zoom: 5,
-//               value: grades[2],
-//             },
-//             radii[2],
-//           ],
-//         ],
-//       },
-//       "circle-color": {
-//         property: "Attendance",
-//         stops: [
-//           [grades[0], colors[0]],
-//           [grades[1], colors[1]],
-//           [grades[2], colors[2]],
-//         ],
-//       },
-//       "circle-stroke-color": "white",
-//       "circle-stroke-width": 1,
-//       "circle-opacity": 0.6,
-//     },
-//   },
-// };
+  layerOptions: {
+    id: "do_not_change_this_valu",
+    type: "fill",
+    source: "do_not_change_this_value",
+    paint: {
+        'fill-color': [
+            'step',
+            ['get', 'total_players'],
+            '#c6dbef',   // stop_output_0
+            50,          // stop_input_0
+            '#9ecae1',   // stop_output_1
+            100,          // stop_input_1
+            '#6baed6',   // stop_output_2
+            150,          // stop_input_2
+            '#4292c6',   // stop_output_3
+            200,         // stop_input_3
+            '#2171b5',   // stop_output_4
+            250,         // stop_input_4
+            '#08306b'   // stop_output_5
+    
+        ],
+        'fill-outline-color': '#BBBBBB',
+        'fill-opacity': 0.8,
+    }
+  },
+};
